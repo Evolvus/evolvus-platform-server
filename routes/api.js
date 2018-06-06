fc7d2a1d84277fa34703bc95a047ecdcc882dd94
KmKavya <kavyavshaiva@gmail.com>
2018-06-06 19:00:34 +0530

    added api to save menuGroup

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,10 @@
 const debug = require("debug")("evolvus-platform-server:routes:api");
 const _ = require("lodash");
 const application = require("evolvus-application");
+const menu = require("evolvus-menu");
 
 const applicationAttributes = ["applicationName", "applicationId", "description", "enabled", "applicationCode", "createdBy", "createdDate", "logo", "favicon"];
+const menuGroupAttributes = ["menuGroupId", "menuGroupCode", "title", "icon", "menuGroupType", "applicationCode"];
 
 module.exports = (router) => {
   router.route('/saveApplication')
@@ -37,17 +39,17 @@ module.exports = (router) => {
 
   router.route('/findByCodeAndEnabled')
     .get((req, res, next) => {
-        try {
-          let codeValue = _.pick(req.body, ['code','enable']);
-          application.findByCodeAndEnabled(codeValue,enable).then((app) => {
-            res.send(app);
-          }).catch((e) => {
-            res.status(400).send(e);
-          });
-        }catch (e) {
-          res.send(e);
-        }
-      });
+      try {
+        let codeValue = _.pick(req.body, ['code', 'enable']);
+        application.findByCodeAndEnabled(codeValue, enable).then((app) => {
+          res.send(app);
+        }).catch((e) => {
+          res.status(400).send(e);
+        });
+      } catch (e) {
+        res.send(e);
+      }
+    });
 
   router.route('/getAllApplications')
     .get((req, res, next) => {
@@ -76,6 +78,34 @@ module.exports = (router) => {
         });
       } catch (e) {
         res.status(400).send(e);
+      }
+    });
+
+  router.route("/saveMenuGroup")
+    .post((req, res, next) => {
+      try {
+        let body = _.pick(req.body, menuGroupAttributes);
+        application.FindByCode(body.applicationCode).then((app) => {
+          if (_.isEmpty(app)) {
+            throw new Error(`No Application with ${body.applicationCode} found`);
+          } else {
+            menu.saveMenuGroup(body).then((menu) => {
+              res.send(menu);
+            }).catch((e) => {
+              res.status(400).send({
+                error: e.message
+              });
+            });
+          }
+        }).catch((e) => {
+          res.status(400).send({
+            error: e.message
+          });
+        })
+      } catch (e) {
+        res.status(400).send({
+          error: e.message
+        });
       }
     });
 };
