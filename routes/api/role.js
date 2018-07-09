066caae0703c6c3fbf34e58ddae0be7a133c269f
pavithra30 <EVOLVUS\kamalaranip@EPCBLR-044.evolvus.com>
2018-07-09 17:00:49 +0530

    added apis to role

--- routes/api/role.js
+++ routes/api/role.js
@@ -1,7 +1,7 @@
 const debug = require("debug")("evolvus-platform-server:routes:api:role");
 const _ = require("lodash");
-const role = require("evolvus-role");
-const application = require("evolvus-new-application")
+const role = require("./../../index");
+const application = require("evolvus-application")
 
 const LIMIT = process.env.LIMIT || 10;
 const tenantHeader = "X-TENANT-ID";
@@ -13,6 +13,7 @@ const PAGE_SIZE = 10;
 
 const roleAttributes = ["tenantId", "roleName", "applicationCode", "description", "activationStatus", "processingStatus", "associatedUsers", "createdBy", "createdDate", "menuGroup", "lastUpdatedDate", "entityId", "accessLevel"];
 const filterAttributes = role.filterAttributes;
+console.log("filterAttributes", filterAttributes);
 const sortableAttributes = role.sortableAttributes;
 
 
@@ -31,6 +32,7 @@ module.exports = (router) => {
         "data": {}
       };
       let body = _.pick(req.body, roleAttributes);
+      console.log("BODY", body);
       try {
         body.associatedUsers = 5;
         body.tenantId = tenantId;
@@ -38,11 +40,13 @@ module.exports = (router) => {
         body.entityId = entityId;
         body.createdDate = new Date().toISOString();
         body.lastUpdatedDate = body.createdDate;
+
         Promise.all([application.find(tenantId, {
           "applicationCode": body.applicationCode
         }, {}, 0, 1), role.find(tenantId, {
           "roleName": body.roleName
         }, {}, 0, 1)]).then((result) => {
+          console.log("RESULT", result);
           if (_.isEmpty(result[0])) {
             throw new Error(`No Application with ${body.applicationCode} found`);
           }
@@ -88,21 +92,26 @@ module.exports = (router) => {
         "description": "",
         "data": {}
       };
+      console.log("query", req.query);
       debug("query: " + JSON.stringify(req.query));
       var limit = _.get(req.query, "limit", LIMIT);
       var pageSize = _.get(req.query, "pageSize", PAGE_SIZE);
       var pageNo = _.get(req.query, "pageNo", 1);
       var skipCount = pageSize * (pageNo - 1);
+      console.log("SKIPCOUNT", skipCount);
       var filter = _.pick(req.query, filterAttributes);
+      console.log("filter", filter);
       var sort = _.get(req.query, "sort", {});
       var orderby = sortable(sort);
       try {
         Promise.all([role.find(tenantId, filter, orderby, skipCount, +limit), role.counts(tenantId, entityId, accessLevel, filter)])
           .then((result) => {
+            console.log("result", result);
             if (result[0].length > 0) {
               response.status = "200";
               response.description = "SUCCESS";
               response.totalNoOfPages = Math.ceil(result[1] / pageSize);
+              console.log("response.totalNoOfPages", response.totalNoOfPages);
               response.totalNoOfRecords = result[1];
               response.data = result[0];
               res.status(200)
@@ -114,57 +123,82 @@ module.exports = (router) => {
               res.status(response.status)
                 .send(JSON.stringify(response, null, 2));
             }
-          })
-          .catch((e) => {
+          }).catch((e) => {
+            console.log("catch1", e);
             debug(`failed to fetch all roles ${e}`);
-            response.status = "400",
-              response.description = `Unable to fetch all roles`
-            response.data = e.toString()
+            response.status = "400";
+            response.description = `Unable to fetch all roles`;
+            response.data = e.toString();
             res.status(response.status).send(JSON.stringify(response, null, 2));
           });
       } catch (e) {
+        console.log("catch2", e);
         debug(`caught exception ${e}`);
-        response.status = "400",
-          response.description = `Unable to fetch all roles`
-        response.data = e.toString()
+        response.status = "400";
+        response.description = `Unable to fetch all roles`;
+        response.data = e.toString();
         res.status(response.status).send(JSON.stringify(response, null, 2));
       }
     });
 
-  router.route("/role")
+  router.route("/role/:roleName")
     .put((req, res, next) => {
+      const tenantId = req.header(tenantHeader);
+      const createdBy = req.header(userHeader);
+      const ipAddress = req.header(ipHeader);
+      const accessLevel = req.header(accessLevelHeader);
+      const entityId = req.header(entityIdHeader)
+      const response = {
+        "status": "200",
+        "description": "",
+        "data": {}
+      };
+      debug("query: " + JSON.stringify(req.query));
       try {
-        let body = _.pick(req.body.roleData, roleAttributes);
-        Promise.all([application.getOne("applicationCode", body.applicationCode), role.getOne("roleName", body.roleName)])
+        let body = _.pick(req.body, roleAttributes);
+        body.updatedBy = req.header(userHeader);;
+        body.lastUpdatedDate = new Date().toISOString();
+        console.log(body, "booodyyyy");
+        role.find(tenantId, {
+            "roleName": body.roleName,
+            "applicationCode": body.applicationCode
+          }, {}, 0, 1)
           .then((result) => {
-            if (_.isEmpty(result[0])) {
-              throw new Error(`No Application with ${body.applicationCode} found`);
+            console.log("RESULT", result);
+            if (!_.isEmpty(result[0])) {
+              throw new Error(`Role ${body.roleName},  already exists `);
             }
-            if ((!_.isEmpty(result[1])) && (result[1]._id != req.params.id)) {
-              throw new Error(`RoleName ${body.roleName} already exists`);
+            if ((!_.isEmpty(result[0])) && (result[0].applicationCode != req.params.applicationCode)) {
+              throw new Error(`Role ${body.roleName} already exists`);
             }
-            let updated = {
-              deletedFlag: "1"
-            };
-            role.update(req.params.id, updated).then((updatedRole) => {
-              res.json(updatedRole);
+            role.update(tenantId, body.roleName, body).then((updatedRoles) => {
+              response.status = "200";
+              response.description = `${body.roleName} Role has been modified successful and sent for the supervisor authorization.`;
+              response.data = body;
+              res.status(200)
+                .send(JSON.stringify(response, null, 2));
             }).catch((e) => {
-              res.status(400).json({
-                error: e.toString()
-              });
+              console.log("CATCH1", e);
+              response.status = "400";
+              response.description = `Unable to modify role ${body.roleName}. Due to ${e.message}`;
+              response.data = e.toString();
+              res.status(response.status).send(JSON.stringify(response, null, 2));
             });
           }).catch((e) => {
-            res.status(400).json({
-              error: e.toString()
-            });
+            console.log("CATCH2", e);
+            response.status = "400";
+            response.description = `Unable to modify role ${body.roleName}. Due to ${e.message}`;
+            response.data = e.toString();
+            res.status(response.status).send(JSON.stringify(response, null, 2));
           });
       } catch (e) {
-        res.status(400).json({
-          error: e
-        });
+        console.log("TRY CATCH", e);
+        response.status = "400";
+        response.description = `Unable to modify role ${body.roleName}. Due to ${e.message}`;
+        response.data = e.toString();
+        res.status(response.status).send(JSON.stringify(response, null, 2));
       }
     });
-
 };
 
 
