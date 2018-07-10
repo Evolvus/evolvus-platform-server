5606a4382bef440ccd3f5862976fdd743c46226f
pavithra30 <pavithra.t@evolvussolutions.com>
2018-07-10 10:28:49 +0530

    added menu apis

--- routes/api/menu.js
+++ routes/api/menu.js
@@ -1,84 +1,138 @@
 const debug = require("debug")("evolvus-platform-server:routes:api:menu");
 const _ = require("lodash");
 const menu = require("evolvus-menu");
-const application = require("evolvus-application");
+
+const LIMIT = process.env.LIMIT || 10;
+const ORDER_BY = process.env.ORDER_BY || {
+  lastUpdatedDate: -1
+};
+const tenantHeader = "X-TENANT-ID";
+const userHeader = "X-USER";
+const ipHeader = "X-IP-HEADER";
+const PAGE_SIZE = 10;
 
 const menuAttributes = ["menuGroupCode", "title", "applicationCode", "tenantId", "menuItems", "createdBy", "createdDate", "menuGroupOrder"];
+const filterAttributes = ["applicationCode"];
+
 
 module.exports = (router) => {
-  router.route("/menu")
-    .post((req, res, next) => {
-      try {
-        let body = _.pick(req.body, menuAttributes);
-        body.createdBy = "SYSTEM";
-        body.createdDate = new Date().toISOString();
-        application.getOne({
-          applicationCode: body.applicationCode
-        }).then((app) => {
-          if (_.isEmpty(app)) {
-            throw new Error(`No Application with ${body.applicationCode} found`);
-          } else {
-            menu.save(body).then((menuObj) => {
-              res.send(menuObj);
-            }).catch((e) => {
-              res.status(400).json({
-                error: e.toString()
-              });
-            });
-          }
-        }).catch((e) => {
-          res.status(400).json({
-            error: e.toString()
-          });
-        });
-      } catch (e) {
-        res.status(400).json({
-          error: e.toString()
-        });
-      }
-    });
 
   router.route('/menu')
     .get((req, res, next) => {
+      const tenantId = req.header(tenantHeader);
+      const createdBy = req.header(userHeader);
+      const ipAddress = req.header(ipHeader);
+      const response = {
+        "status": "200",
+        "description": "",
+        "data": {}
+      };
+      debug("query: " + JSON.stringify(req.query));
+      var limit = _.get(req.query, "limit", LIMIT);
+      var pageSize = _.get(req.query, "pageSize", PAGE_SIZE);
+      var pageNo = _.get(req.query, "pageNo", 1);
+      var skipCount = pageSize * (pageNo - 1);
+      var filterValues = _.pick(req.query, filterAttributes);
+      var filter = _.omitBy(filterValues, function(value, key) {
+        return value.startsWith("undefined");
+      });
+      var sort = _.get(req.query, "sort", {});
+      var orderby = sortable(sort);
       try {
-        menu.getAll(-1).then((menus) => {
-          if (menus.length > 0) {
-            res.json(menus);
-          } else {
-            res.status(204).json({
-              message: "No menus found"
-            });
-          }
-        }).catch((e) => {
-          res.status(400).json({
-            error: e.toString()
+        menu.find(tenantId, filter, orderby, skipCount, +limit)
+          .then((menus) => {
+            if (menus.length > 0) {
+              response.status = "200";
+              response.description = "SUCCESS";
+              response.data = menus;
+              res.status(200)
+                .send(JSON.stringify(response, null, 2));
+            } else {
+              response.status = "204";
+              response.data=[];
+              response.description = "No menus found";
+              debug("response: " + JSON.stringify(response));
+              res.status(200)
+                .send(JSON.stringify(response, null, 2));
+            }
+          })
+          .catch((e) => {
+            debug(`failed to fetch all menus ${e}`);
+            response.status = "400";
+            response.description = `Unable to fetch all menus`;
+            response.data = e.toString();
+            res.status(response.status).send(JSON.stringify(response, null, 2));
           });
-        });
       } catch (e) {
-        res.status(400).json({
-          error: e.toString()
-        });
+        debug(`caught exception ${e}`);
+        response.status = "400";
+            response.description = `Unable to fetch all menus`;
+            response.data = e.toString();
+            res.status(response.status).send(JSON.stringify(response, null, 2));
       }
     });
 
-  router.route('/menu/find')
-    .get((req, res, next) => {
+  router.route('/menu/')
+    .post((req, res, next) => {
+      const tenantId = req.header(tenantHeader);
+      const createdBy = req.header(userHeader);
+      const ipAddress = req.header(ipHeader);
+      const response = {
+        "status": "200",
+        "description": "",
+        "data": {}
+      };
+      debug("query: " + JSON.stringify(req.query));
+      let body = _.pick(req.body, menuAttributes);
+      body.createdDate = new Date().toISOString();
+      body.lastUpdatedDate = body.createdDate;
       try {
-        let codeValue = req.query.applicationCode;
-        menu.getMany("applicationCode", codeValue).then((app) => {
-          res.json(app);
-        }).catch((e) => {
-          res.status(400).json({
-            error: e.toString()
+        menu.save(tenantId, body)
+          .then((menus) => {
+            response.status = "200";
+            response.description = "SUCCESS";
+            response.data = menus;
+            res.status(200)
+              .send(JSON.stringify(response, null, 2));
+          })
+          .catch((e) => {
+            debug(`failed to fetch all menus ${e}`);
+            response.status = "400";
+            response.description = `Unable to add new  menus.Due to ${e.message}`;
+            response.data = e.toString();
+            res.status(response.status).send(JSON.stringify(response, null, 2));
           });
-        });
       } catch (e) {
-        res.status(400).json({
-          error: e.toString()
-        });
+        debug(`failed to fetch all menus ${e}`);
+        response.status = "400";
+        response.description = `Unable to add new  menus.Due to ${e.message}`;
+        response.data = e.toString();
+        res.status(response.status).send(JSON.stringify(response, null, 2));
       }
     });
-
-
-
 };
+
+
+function sortable(sort) {
+  if (typeof sort === 'undefined' ||
+    sort == null) {
+    return ORDER_BY;
+  }
+  if (typeof sort === 'string') {
+    var result = sort.split(",")
+      .reduce((temp, sortParam) => {
+        if (sortParam.charAt(0) == "-") {
+          return _.assign(temp, _.fromPairs([
+            [sortParam.replace(/-/, ""), -1]
+          ]));
+        } else {
+          return _.assign(_.fromPairs([
+            [sortParam.replace(/\+/, ""), 1]
+          ]));
+        }
+      }, {});
+    return result;
+  } else {
+    return ORDER_BY;
+  }
+}
